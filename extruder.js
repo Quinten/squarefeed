021a1a37925551e352bb2b7f7c4cf6f774127a22
Quinten Clause <quinten.clause@studioemma.eu>
2017-12-16 18:43:07 +0100

    Add extrusion -y

--- extruder.js
+++ extruder.js
@@ -20,10 +20,25 @@ squares.push({a: [2, 5, 7], b: [2, 3, 5], d: '-y', e: false}); // top
 squares.push({a: [8, 4, 1], b: [8, 6, 4], d: '+y', e: false}); // bottom
 squares.push({a: [6, 7, 5], b: [6, 8, 7], d: '+z', e: false}); // back
 
+function checkVertices(newVertices) {
+    for (var n = 0; n < newVertices.length; n++) {
+        for (var v = 0; v < vertices.length; v++) {
+            if (vertices[v].x == newVertices[n].x && vertices[v].y == newVertices[n].y && vertices[v].z == newVertices[n].z) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
 var size = 8;
 
 // create extrusions
 function createExtrusion(square) {
+
+    if (square.e) {
+        return;
+    }
 
     var newVertices = [];
     switch (square.d) {
@@ -32,11 +47,25 @@ function createExtrusion(square) {
             newVertices.push({x: vertices[square.a[1]].x, y: vertices[square.a[1]].y - size, z: vertices[square.a[1]].z});
             newVertices.push({x: vertices[square.a[2]].x, y: vertices[square.a[2]].y - size, z: vertices[square.a[2]].z});
             newVertices.push({x: vertices[square.b[1]].x, y: vertices[square.b[1]].y - size, z: vertices[square.b[1]].z});
+            if (checkVertices(newVertices)) {
+                var vertexIndex = vertices.length;
+                vertices.push(...newVertices);
+                //console.log(vertices);
+                squares.push({a: [vertexIndex, vertexIndex + 1, vertexIndex + 2], b: [vertexIndex, vertexIndex + 3, vertexIndex + 1], d: '-y', e: false}); // top
+                squares.push({a: [square.a[0], vertexIndex + 3, vertexIndex], b: [square.a[0], square.b[1], vertexIndex + 3], d: '-z', e: false}); // front
+                squares.push({a: [square.a[1], vertexIndex + 2, vertexIndex + 1], b: [square.a[1], square.a[2], vertexIndex + 2], d: '+z', e: false}); // back
+                squares.push({a: [square.a[2], vertexIndex, vertexIndex + 2], b: [square.a[2], square.a[0], vertexIndex], d: '-x', e: false}); // left
+                squares.push({a: [square.b[1], vertexIndex + 1, vertexIndex + 3], b: [square.b[1], square.a[1], vertexIndex + 1], d: '+x', e: false}); // right
+                square.e = true;
+            } else {
+                return;
+            }
             break;
     }
 }
 
 createExtrusion(squares[3]);
+createExtrusion(squares[6]);
 
 // write to file
 var data = '#extrusions\n';
@@ -46,8 +75,10 @@ for (var v = 1; v < vertices.length; v++) {
 }
 
 for (var s = 0; s < squares.length; s++) {
-    data += 'f ' + squares[s].a[0] + ' ' + squares[s].a[1] + ' ' + squares[s].a[2] + '\n';
-    data += 'f ' + squares[s].b[0] + ' ' + squares[s].b[1] + ' ' + squares[s].b[2] + '\n';
+    if (!squares[s].e) {
+        data += 'f ' + squares[s].a[0] + ' ' + squares[s].a[1] + ' ' + squares[s].a[2] + '\n';
+        data += 'f ' + squares[s].b[0] + ' ' + squares[s].b[1] + ' ' + squares[s].b[2] + '\n';
+    }
 }
 
 var fs = require('fs');
